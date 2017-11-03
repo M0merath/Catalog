706399c94fa8f6d4066a1ef2a1e9c9c7c914c5ab
Kevin-Lanzing <klanzing@gmail.com>
2017-11-03 17:47:22 -0400

    Fixed indent error on line 313

--- catalog.py
+++ catalog.py
@@ -308,16 +308,16 @@ def recipesJSON(category_id):
 
 @app.route('/category/new', methods=['GET', 'POST'])
 def newCategory():
-	if 'username' not in login_session:
-            return redirect('/login')
-        if request.method == 'POST':
-            newCategory = Category(name=request.form['name'], user_id=login_session['user_id'])
-            session.add(newCategory)
-            flash('New Category %s Successfully Created' % newCategory.name)
-            session.commit()
-            return redirect(url_for('showCategories'))
-        else:
-            return render_template('newCategory.html')
+    if 'username' not in login_session:
+        return redirect('/login')
+    if request.method == 'POST':
+        newCategory = Category(name=request.form['name'], user_id=login_session['user_id'])
+        session.add(newCategory)
+        flash('New Category %s Successfully Created' % newCategory.name)
+        session.commit()
+        return redirect(url_for('showCategories'))
+    else:
+        return render_template('newCategory.html')
 
 
 @app.route('/category/<int:category_id>/edit', methods=['GET', 'POST'])
